94971c2ce2273babd6107e3f7a4380946800b9cb
rival0001 <117112503+rival0001@users.noreply.github.com>
2023-08-15 06:07:59 +0530

    first project1

--- Guessing_Game/Guessing_Game.cpp
+++ Guessing_Game/Guessing_Game.cpp
@@ -3,28 +3,30 @@
 #include <cstdlib>
 #include <ctime>
 
+using namespace std;
+
 int main() {
-    srand(time(0));  // Seed the random number generator with the current time
-    
+    srand(time(0)); // Seed the random number generator with the current time
+
     int secretNumber = rand() % 100 + 1;  // Generate a random number between 1 and 100
     
-    std::cout << "Welcome to the Number Guessing Game!" << std::endl;
-    std::cout << "I'm thinking of a number between 1 and 100." << std::endl;
+   cout << "Welcome to the Number Guessing Game!" <<endl;
+   cout << "I'm thinking of a number between 1 and 100." << endl;
     
     int guess = 0;
     int attempts = 0;
     
     while (guess != secretNumber) {
-        std::cout << "Take a guess: ";
-        std::cin >> guess;
+        cout << "Take a guess: ";
+        cin >> guess;
         attempts++;
         
         if (guess < secretNumber) {
-            std::cout << "Too low! Try a higher number." << std::endl;
+            cout << "Too low! Try a higher number." << endl;
         } else if (guess > secretNumber) {
-            std::cout << "Too high! Try a lower number." << std::endl;
+            cout << "Too high! Try a lower number." << endl;
         } else {
-            std::cout << "Congratulations! You guessed the number " << secretNumber << " in " << attempts << " attempts." << std::endl;
+            cout << "Congratulations! You guessed the number " << secretNumber << " in " << attempts << " attempts." << endl;
         }
     }
     
